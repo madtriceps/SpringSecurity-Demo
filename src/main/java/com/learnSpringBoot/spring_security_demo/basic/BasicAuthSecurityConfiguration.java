90eb3a49472dded124d46cbc12dc2288c0f2b079
madtriceps <115035770+madtriceps@users.noreply.github.com>
2024-11-14 22:18:56 +0530

    Final Spring Security Commit !

--- src/main/java/com/learnSpringBoot/spring_security_demo/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/learnSpringBoot/spring_security_demo/basic/BasicAuthSecurityConfiguration.java
@@ -6,20 +6,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BasicAuthSecurityConfiguration {
 	@Bean
 	SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests(
+				(requests) ->{
+					requests
+//					.requestMatchers("/users").hasRole("USER")
+//					.requestMatchers("/admin/**").hasRole("ADMIN")
+					.anyRequest().authenticated();
+				
+				});
 //		http.formLogin(withDefaults());
 		http.sessionManagement(
 				session -> 
@@ -59,17 +70,23 @@ public class BasicAuthSecurityConfiguration {
 	@Bean
 	public UserDetailsService userDetailService(DataSource dataSource) {
 		var user = User.withUsername("Maddy")
-				.password("{noop}dummy")
+				//.password("{noop}dummy")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
 				.roles("USER")
 				.build();
 		
 		var admin = User.withUsername("admin")
-				.password("{noop}dummy")
+				//.password("{noop}dummy")
+				.password("dummy")
+			    .passwordEncoder(str -> passwordEncoder().encode(str))
 				.roles("ADMIN")
 				.build();
 		
 		var superAdmin = User.withUsername("super")
-				.password("{noop}dummy")
+				//.password("{noop}dummy")
+				.password("dummy")
+			    .passwordEncoder(str -> passwordEncoder().encode(str))
 				.roles("ADMIN","USER")
 				.build();
 		
@@ -82,4 +99,11 @@ public class BasicAuthSecurityConfiguration {
 //		return new InMemoryUserDetailsManager(user,admin);
 		}
 	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+		
 }
+
--- src/main/java/com/learnSpringBoot/spring_security_demo/resources/TodoResource.java
+++ src/main/java/com/learnSpringBoot/spring_security_demo/resources/TodoResource.java
@@ -1,11 +1,15 @@
 package com.learnSpringBoot.spring_security_demo.resources;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.LoggerFactoryFriend;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +24,12 @@ public class TodoResource {
     private static final List<Todo> TODOS_LIST = List.of(
         new Todo("Maddy", "Learn DSA"),
         new Todo("Maddy", "Get a dream job"),
-        new Todo("John", "Complete Spring Boot tutorial")
+        new Todo("John", "Complete Spring Boot tutorial"),
+        new Todo("John", "Complete Spring Boot tutorial 1"),
+        new Todo("John", "Complete Spring Boot tutorial 2"),
+        new Todo("John", "Complete Spring Boot tutorial 3"),
+        new Todo("super", "Complete Spring Boot tutorial for super user"),
+        new Todo("admin", "Complete Spring Boot tutorial for admin")
     );
 
     @GetMapping("/todos")
@@ -29,11 +38,32 @@ public class TodoResource {
     }
     
     @GetMapping("/users/{username}/todos")
-    public List<Todo> retrieveTodosForSpecificUser(@PathVariable("username") String username) {
+//    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PreAuthorize("hasAuthority('ROLE_USER') and (#username == null or #username.equals(authentication.name))")
+
+    public List<Todo> retrieveTodosForSpecificUser(@PathVariable("username") String username, Authentication auth) {
+    	if(auth.getName().equals(username)) {
+    		System.out.println("YESS");
+    		if(auth.getName().toString() == username.toString()) {
+    			System.out.println("yeah...this too");
+    		  }
+    	}
+    	else {
+    		System.out.println("NOOOOOOO" + "Username:"+username+", authname:"+auth.getName());
+    	}
         return TODOS_LIST.stream()
                          .filter(todo -> todo.username().equals(username))
                          .collect(Collectors.toList());
     }
+
+
+    
+    @ExceptionHandler(AccessDeniedException.class)
+    public void handleAccessDenied(AccessDeniedException ex, Authentication authentication) {
+        System.out.println("Access denied for user: " + authentication.getName());
+        System.out.println("Attempted access to resource by username: " + ex.getMessage());
+    }
+    
     
     @PostMapping("/users/{username}/todos")
     public void createTodosForSpecificUser(@PathVariable("username") String username,
